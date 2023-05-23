275c07f839e226b8e51b5b05b80d87afe2edb264
Perry Jorss <pjorss123@gmail.com>
2023-05-22 21:46:29 -0400

    commit tree 3d1644c7b3a96f5a79a4150ff9634316e029e49b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def get_first
+    first_student = Student.find(params[:id])
+    render json: first_student
+  end
+
 end
